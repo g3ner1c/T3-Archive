52070d5ade505e2c5f2d704b4154e8e8ad0691db
Rocky <74469257+awesomeplaya211@users.noreply.github.com>
2021-12-26 23:53:18 -0500

    Reformatted jsons and made it into a website

---
+++ script.js
@@ -0,0 +1,27 @@
+import data from "./scrims/scrims.json" assert { type: "json" };
+
+// document.getElementById("content").innerText = JSON.stringify(data);
+
+var html = "";
+
+for (var date of data) {
+
+    html += `<h3>${date["date"]}</h3>`;
+
+    for (var map of date["matches"]) {
+
+        html += `<h4>${map["map"]}</h4>`;
+
+        for (var log in map["logs"]) {
+
+            html += `<h5><a href="https://logs.tf/${map["logs"][log]}">Logs</a>&nbsp;&nbsp;&nbsp;&nbsp;`;
+
+            html += `<a href="https://demos.tf/${map["demos"][log]}">Demos</a></h5>`;
+
+        }
+
+    }
+
+}
+
+document.getElementById("scrims").innerHTML = html;
